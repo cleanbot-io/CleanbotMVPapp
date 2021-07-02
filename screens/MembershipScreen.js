461720a38afe8427bb46c5fa5a7a5653d17ea66b
Knighton910 <keltheceo@gmail.com>
2021-07-02 18:25:50 -0500

    added SafeAreaView and ScrollView

--- screens/MembershipScreen.js
+++ screens/MembershipScreen.js
@@ -1,49 +1,91 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
-import {Button, Input, Card } from 'react-native-elements'
+import { StyleSheet, Text, View, Image, SafeAreaView, ScrollView } from 'react-native'
+import {Button, Input, Card, ListItem, Avatar, Icon } from 'react-native-elements'
 
 function MembershipScreen() {
     return (
-        <View styles={styles.container}>
-            <Text style={{textAlign: 'center', fontSize: 20, marginTop: 7}}>Subscribe to a plan</Text>
+        <SafeAreaView styles={styles.container}>
+            <ScrollView>
+                <View>
+                    <Text style={{textAlign: 'center', fontSize: 20, marginTop: 7}}>Subscribe to a plan</Text>
 
-            <View  styles={styles.basicCard}>
-                <Card borderRadius={20}>
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={{fontSize: 23, color: 'red'}}>Basic</Text><Text style={{marginLeft: 15, fontSize: 36}}>$52</Text>
+                    <View  styles={styles.basicCard}>
+                        <Card borderRadius={20}>
+                            <View style={{flexDirection: 'row'}}>
+                                <Button title="Basic"/>
+                                <Text style={{marginLeft: 15, fontSize: 36}}>$52</Text>
+                            </View>
+                            <Card.Divider/>
+                            <ListItem>
+                                <Text>Features</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                        </Card>
                     </View>
-                    <Card.Divider/>
-                    <Text style={{color: 'red'}}>Features</Text>
-                </Card>
-            </View>
 
-            <View  styles={styles.standardCard}>
-                <Card borderRadius={20}>
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={{fontSize: 23, color: 'blue'}}>Standard</Text><Text style={{marginLeft: 15, fontSize: 36}}>$94</Text>
+                    <View  styles={styles.standardCard}>
+                        <Card borderRadius={20}>
+                            <View style={{flexDirection: 'row'}}>
+                            <Button title="Standard"/>
+                                <Text style={{marginLeft: 15, fontSize: 36}}>$94</Text>
+                            </View>
+                            <Card.Divider/>
+                            <ListItem>
+                                <Text>Features</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                        </Card>
                     </View>
-                    <Card.Divider/>
-                    <Text style={{color: 'blue'}}>Features</Text>
-                </Card>
-            </View>
 
-            <View  styles={styles.premiumCard}>
-                <Card borderRadius={20}>
-                    <View style={{flexDirection: 'row'}}>
-                        <Text style={{fontSize: 23, color: '#efc583'}}>Premium</Text><Text style={{marginLeft: 15, fontSize: 36}}>$178</Text>
+                    <View  styles={styles.premiumCard}>
+                        <Card borderRadius={20}>
+                            <View style={{flexDirection: 'row'}}>
+                            <Button title="Premium" />
+                                <Text style={{marginLeft: 15, fontSize: 36}}>$178</Text>
+                            </View>
+                            <Card.Divider/>
+                            <ListItem>
+                                <Text>Features</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                            <ListItem>
+                                <Icon name='check'/><Text>Four cleanings per month</Text>
+                            </ListItem>
+                        </Card>
                     </View>
-                    <Card.Divider/>
-                    <Text style={{color: '#efc583'}}>Features</Text>
-                </Card>
-            </View>
-        </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 export default MembershipScreen
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        flex: 1,
+    },
     standardHead: {},
     standardTitle: {
         fontSize: 24,
